71c32c69bf17663512535944ddcdd5f84ef73858
manhdoi <manhtran40kc@gmail.com>
2024-08-02 21:44:43 +0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+version = "0.0.1"
+
+setup(
+    name='m-tml',
+    version=version,
+    packages=['tml', 'tml.common', 'tml.lexical', 'tml.parsing', 'tml.testing', 'tml.exceptions'],
+    url='',
+    license='',
+    author='manhdoi',
+    author_email='manhtran40kc@gmail.com',
+    description=''
+)
